d219f5a8b26832dc3b0eeb6285670f319d799927
abdulaziz27067011 <abdullox30007@gmail.com>
2025-02-21 13:23:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let nums = [2, 4, 6, 7, 9, 12, 15, 16, 30, 37, 85, 98, 100000]
+
+let newArr = []
+
+for (const element of nums) {
+
+    if (element % 2 == 0) {
+        console.log (`Bu juft sonlar: ${element}`)
+        newArr.push(element)
+    }
+
+}
+
+console.log(newArr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
